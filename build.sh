f2203ca52de1e15792a44b0d36c22be13cec81a6
Yet Another Solutions <yet@another.solutions>
2024-06-03 18:57:37 +0300

    privileged container for mount

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 pushd builder
 ./builder.sh
 popd
-docker run --rm -i --mount "type=bind,src=./dist,dst=/dist" "localhost/builder" /dist/build.sh 
+docker run --rm -i --privileged --mount "type=bind,src=./dist,dst=/dist" "localhost/builder" /dist/build.sh 
