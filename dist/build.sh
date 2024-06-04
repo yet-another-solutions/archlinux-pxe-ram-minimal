3996b1ea33b3644a128b31cad79b53c4641d0925
Yet Another Solutions <yet@another.solutions>
2024-06-04 12:12:15 +0300

    try fix

--- dist/build.sh
+++ dist/build.sh
@@ -25,15 +25,17 @@ mkdir -p /mnt/main/squash_root
 mksquashfs /mnt/main/new_root /mnt/main/squash_root/rootfs
 
 pushd /mnt/main/squash_root
-ls
-echo "looking"
-LC_ALL=C.UTF-8 find * -mindepth 1 -name "rootfs" -printf '%P\0'
-echo "looked"
-echo "debug find"
-find * -mindepth 1 -name "rootfs"
-echo "debug found"
-LC_ALL=C.UTF-8 find * -mindepth 1 -name "rootfs" -printf '%P\0' | LC_ALL=C.UTF-8 sort -z | LC_ALL=C.UTF-8 bsdtar --uid 0 --gid 0 --null -cnf - -T - | LC_ALL=C.UTF-8 bsdtar --null -cf - --format=newc @- | zstd >> /dist/rootfs.img
+#ls
+#echo "looking"
+#LC_ALL=C.UTF-8 find * -mindepth 1 -name "rootfs" -printf '%P\0'
+#echo "looked"
+#echo "debug find"
+#find * -mindepth 1 -name "rootfs"
+#echo "debug found"
+LC_ALL=C.UTF-8 find * -mindepth 0 -name "rootfs" -printf '%P\0' | LC_ALL=C.UTF-8 sort -z | LC_ALL=C.UTF-8 bsdtar --uid 0 --gid 0 --null -cnf - -T - | LC_ALL=C.UTF-8 bsdtar --null -cf - --format=newc @- | zstd >> /dist/rootfs.img
 popd
 
 cp /mnt/main/new_root/boot/vmlinuz-linux /dist/kernel.img
 cp /mnt/main/new_root/boot/initramfs-linux.img /dist/initramfs.img
+
+chmod 666 /dist/*.img
