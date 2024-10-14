874397db284eca7e59787b9a20747e6d65b913df
yet-another-solutions <115307245+yet-another-solutions@users.noreply.github.com>
2024-10-14 17:19:39 +0300

    Package installation moved to workflow

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-apt-get install xorriso -y
-apt-get install syslinux -y
-apt-get install isolinux -y
-apt-get install grub2-common -y
-apt-get install grub-pc-bin -y
-apt-get install grub-efi-ia32-bin -y
-apt-get install grub-efi-amd64-bin -y
 pushd builder
 ./builder.sh
 popd
